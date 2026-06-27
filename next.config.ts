4f3f4c128d81a1bb39d0d84625f17d19d49f0705
Gabriel Lima de Oliveira <ogabriellima1999@gmail.com>
2026-06-26 21:55:42 -0300

    fix: desativa COOP para permitir VLibras comunicar com iframe cross-origin

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: ["lh3.googleusercontent.com", "avatars.githubusercontent.com"],
   },
